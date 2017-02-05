433256458719cde8761518b299738bd19b5ccaf9
Patrick Merlot <patrick.merlot@gmail.com>
2017-02-05 21:50:10 +0100

    test with Oslo buses, car traffic at night, many people, bike lane, ...

--- tools/demo.py
+++ tools/demo.py
@@ -124,8 +124,14 @@ if __name__ == '__main__':
     for i in xrange(2):
         _, _= im_detect(sess, net, im)
 
-    im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-                '001763.jpg', '004545.jpg']
+    im_names = ['000456.jpg',
+                #'000542.jpg', '001150.jpg','001550.jpg',
+                #'001763.jpg', '004545.jpg',
+                'team.jpg', 'car_traffic.jpg',
+                'Bus-OsloNorway.jpg',
+                'bike_lane.jpg',
+                'night_traffic.jpg',
+                'startupLab.jpg']
 
 
     for im_name in im_names:
